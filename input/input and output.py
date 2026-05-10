eedeb61a9385377c56d1bd3ab9b8524ae6a98c3f
mohamed taher <mohamed.dahir.dev@gmail.com>
2026-05-11 00:41:12 +0600

    learned unformatted output using f string

--- input/input and output.py
+++ input/input and output.py
@@ -3,7 +3,7 @@ population = 25
 
 # Method 1: comma
 print("Population of", country, "is", population, "Million")
- 
+
 # Method 2: plus sign
 print("Population of " + country + " is " + str(population) + " Million")
 
